7a25a9409c85d2e8c7ffd2c018d558b26cf581c5
Danx016 <danilorodelo355@gmail.com>
2026-09-26 08:28:30 -0500

    fix(maps): use OpenStreetMap tiles to fix Carto API key requirement

--- client/src/components/LocationPickerModal.jsx
+++ client/src/components/LocationPickerModal.jsx
@@ -99,10 +99,9 @@ export default function LocationPickerModal({
           scrollWheelZoom: true
         })
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
-          subdomains: 'abcd',
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map)
 
         const marker = L.marker([lat, lng], {
